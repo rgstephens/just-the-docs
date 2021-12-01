28a930ce81c427e013ba1a74bd5901f3a20ee3c0
Greg Stephens <g.stephens@rasa.com>
2021-12-01 09:53:46 -0800

    workflow

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 VERS=$(yq e '.version' _config.yml)
 time docker buildx build --platform linux/amd64,linux/arm64 --output=type=registry -t ghcr.io/rgstephens/bandonbrawl.com:${VERS} -t ghcr.io/rgstephens/bandonbrawl.com:latest .
-#docker push ghcr.io/rgstephens/golftrip.link --all-tags
+#time docker buildx build --platform linux/arm64 --output=type=registry -t ghcr.io/rgstephens/bandonbrawl.com:${VERS} -t ghcr.io/rgstephens/bandonbrawl.com:latest .
+docker push ghcr.io/rgstephens/bandonbrawl.com --all-tags
 #kubectl config use-context default
 #kubectl rollout restart deploy bandonbrawl -n websites
